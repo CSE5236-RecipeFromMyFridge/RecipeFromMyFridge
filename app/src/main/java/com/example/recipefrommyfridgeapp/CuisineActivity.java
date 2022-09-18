34778ae5f8f9032d41c9e9c5fd22b61248bf6db4
Nora Liu <nora@Noras-MBP.attlocal.net>
2022-09-18 17:12:05 -0400

    Added cuisine xml

---
+++ app/src/main/java/com/example/recipefrommyfridgeapp/CuisineActivity.java
@@ -0,0 +1,14 @@
+package com.example.recipefrommyfridgeapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CuisineActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cuisine);
+    }
+}
